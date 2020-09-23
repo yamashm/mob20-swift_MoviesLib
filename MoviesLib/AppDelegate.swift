ea38b0ba4a5dc3cbbc2e7f51533dab823cf9cfb6
yamashm <awnmist@gmail.com>
2020-09-22 21:24:03 -0300

    Inicial

---
+++ MoviesLib/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MoviesLib
+//
+//  Created by Usuário Convidado on 22/09/20.
+//  Copyright © 2020 FIAP. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        window?.tintColor = UIColor(named: "Main")
+        
+        return true
+    }
+
+    
+
+}
+
---
+++ MoviesLib/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesLib
+//
+//  Created by Usuário Convidado on 22/09/20.
+//  Copyright © 2020 FIAP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
