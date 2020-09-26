b59862d6fa8e2e311a56cbb17331ba6f6efe5d02
yamashm <awnmist@gmail.com>
2020-09-26 10:47:12 -0300

    Aula 03

--- MoviesLib/Main/AppDelegate.swift
+++ MoviesLib/Main/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,8 +21,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
-
     
-
+    // MARK: - Core Data Stack
+       lazy var persistentContainer: NSPersistentContainer = {
+           let container = NSPersistentContainer(name: "MoviesLib")
+           container.loadPersistentStores { (storeDescription, error) in
+               if let error = error {
+                   print(error)
+               }
+           }
+           return container
+       }()
 }
 
